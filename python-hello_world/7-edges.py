4146cd0f5521ab522f5d6924cfdf6bad126e8c69
Tissem-Ben <t.bendahma@gmail.com>
2024-09-06 10:44:23 +0200

    Copy - Cut - Paste

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -2,9 +2,9 @@
 
 word = "Holberton"
 
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
+word_first_3 = word[:3]       # Contains the first 3 letters of `word`
+word_last_2 = word[-2:]       # Contains the last 2 letters of `word`
+middle_word = word[1:-1]      # Contains `word` without the first and last letters
 
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
