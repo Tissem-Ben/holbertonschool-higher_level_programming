8d6920d6e3c9f304c10508b19aa9b2242cad4ac0
Tissem-Ben <t.bendahma@gmail.com>
2024-09-11 14:22:31 +0200

    Add shebang to scripts and correct function implementation

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
-    # Check if the index is within the valid range
     if idx < 0 or idx >= len(my_list):
-        return None  # Return None if index is negative or out of range
-    return my_list[idx]  # Return the element at the given index
+        return None
+    return my_list[idx]
