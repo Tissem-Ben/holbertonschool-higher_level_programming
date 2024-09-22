ce5eae9e071a530d8e62b1d87a79e556568af092
Tissem-Ben <t.bendahma@gmail.com>
2024-09-22 11:16:43 +0200

    Add class Rectangle with area and perimeter methods (based on 1-rectangle.py)

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class."""
+
+
+class Rectangle:
+    """A class that defines a rectangle by its width and height."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle with optional width and height"""
