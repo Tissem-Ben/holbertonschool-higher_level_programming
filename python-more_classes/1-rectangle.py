e4afe3f407901afd40c87d83ebafd9cf2bbbee41
Tissem-Ben <t.bendahma@gmail.com>
2024-09-22 11:08:50 +0200

    Add class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Shebang line indicating the interpreter for the script."""
+
 
 class Rectangle:
+    """A class that defines a rectangle"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
