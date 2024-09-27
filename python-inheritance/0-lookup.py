433a30e4e949f9a43a26d6893f08530951bf2a2f
Tissem-Ben <t.bendahma@gmail.com>
2024-09-27 22:18:55 +0200

    a function that returns the list of available attributes and methods of an object:

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Shebang line indicating the interpreter for the script."""
+
+
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object."""
+    return dir(obj)
