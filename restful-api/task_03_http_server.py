4e26c8ac6bc453ba8663032733ff928c005c943d
Tissem-Ben <t.bendahma@gmail.com>
2024-10-16 11:03:56 +0200

    restful api http server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,25 +1,33 @@
-#!/usr/bin/env python3
-"""Shebang line indicating the interpreter for the script."""
+#!/usr/bin/python3
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-import xml.etree.ElementTree as ET
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
 
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Serving on port {port}...')
+    httpd.serve_forever()
 
-def serialize_to_xml(dictionary, filename):
-    root = ET.Element("data")
-    for key, value in dictionary.items():
-        child = ET.SubElement(root, key)
-        child.text = str(value)
-
-    tree = ET.ElementTree(root)
-    tree.write(filename, encoding='utf-8', xml_declaration=True)
-
-
-def deserialize_from_xml(filename):
-    tree = ET.parse(filename)
-    root = tree.getroot()
-    dictionary = {}
-    for child in root:
-        dictionary[child.tag] = child.text
-    return dictionary
-
-gys
+if __name__ == "__main__":
+    run()
