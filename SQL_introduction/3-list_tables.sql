dde9557c5089395b2b49a29cc1d8cce2e35121b9
Tissem-Ben <t.bendahma@gmail.com>
2024-10-31 18:25:51 +0100

    Correction du fichier 4-first_table.sql pour respecter le style PEP 8

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
 -- This script lists all the tables in a specified database
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema = 'my_database';
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'my_database'
