265292d5780b82dde05201d1b27d2ab33d5a1de6
Tissem-Ben <t.bendahma@gmail.com>
2024-11-03 11:07:05 +0100

    Add unique_id table and test operations

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,11 @@
+-- Création de la table unique_id
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
+
+-- Insertion d'une entrée
+INSERT INTO unique_id (id, name) VALUES (89, "Best School");
+
+-- Autres insertions...
 
