354593cf98cbef216854496b3bb19cc4c9133d23
Tissem-Ben <t.bendahma@gmail.com>
2024-11-05 16:31:30 +0100

    Add SQL script for selecting states

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,10 +1,8 @@
--- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
-SELECT 
-    GRANTEE, 
-    TABLE_CATALOG, 
-    PRIVILEGE_TYPE 
-FROM 
-    information_schema.role_table_grants 
-WHERE 
-    GRANTEE IN ('user_0d_1', 'user_0d_2');
+-- Check user privileges for user_0d_1 and user_0d_2
+SELECT user, host, privilege FROM information_schema.user_privileges
+WHERE user IN ('user_0d_1', 'user_0d_2')
+ORDER BY user, host;
 
+-- Create user_0d_1 with all privileges if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'your_password';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
