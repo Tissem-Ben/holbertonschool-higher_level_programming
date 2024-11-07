e687cf488a2479ab7906dc4fcd95c5985104d55f
Tissem-Ben <t.bendahma@gmail.com>
2024-11-07 09:41:46 +0100

    Fix script to query cities by state with proper MySQL connection

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-import MySQLdb
+"""Sheban script"""
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Récupérer les arguments de la ligne de commande
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    """
+    The main entry point of the script"""
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
 
-    # Connexion à la base de données MySQL
     db = MySQLdb.connect(
-        host="localhost",  # Ajout de la virgule ici
-        user=username,     # Ajout de la virgule ici
-        passwd=password,   # Ajout de la virgule ici
-        db=db_name         # Ajout de la virgule ici
-    )
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_db)
+
     cursor = db.cursor()
-
-    # Exécution de la requête SQL
-    cursor.execute(
-        "SELECT cities.id, cities.name, states.name "
-        "FROM cities "
-        "INNER JOIN states ON cities.state_id = states.id "
-        "ORDER BY cities.id ASC;"
-    )
-
-    # Récupérer les résultats et les afficher
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                   "JOIN states ON cities.state_id = states.id "
+                   "ORDER BY cities.id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-    # Fermeture de la connexion
     cursor.close()
     db.close()
